2ceb54c9ac85e84d12726ab3b4261b2c0bb29a51
Nicholas Ormrod <nicholas.ormrod@hotmail.com>
2012-05-28 19:07:44 -0400

    moved everything to src

---
+++ src/DarkCell.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main (int argc, const char ** argv) {
+  //====================================
+  // Usage
+
+  if (argc < 2) {
+    cerr << "Usage: " << argv[0] << " filename [filename ...]" << endl;
+    return 1;
+  }
+
+  return 4;
+}
+
